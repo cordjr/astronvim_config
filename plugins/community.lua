07e0bf4efa7057fca8b12d1a32dc6b976db6360a
Jose <cordjr@gmail.com>
2024-03-16 09:26:50 +0000

    more custom configs

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,11 @@
-return {
-  -- Add the community repository of plugin specifications
+return { -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.lsp.lsp-lens-nvim" },
+  { import = "astrocommunity.lsp.nvim-lsp-file-operations" },
+  -- { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "python", "elixir", "rust", "go", "html", "css", "python", "yaml", "toml" },
   },
 }
